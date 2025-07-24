828a25e599743d754df1cbb3766d4cfc00a4806e
Vibhore Mathur <onlyvibhore@gmail.com>
2025-07-24 23:49:00 +0530

    axios base instance

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:5000/api',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default axiosInstance;
